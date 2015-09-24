a95ce90eea1278a87b97f4e8e84083b201f4bcdd
TroyWindows <troy.long@utsouthwestern.edu>
2015-09-24 13:49:25 -0500

    Have working column generation code, still need to write solution printer

--- cgModel.py
+++ cgModel.py
@@ -9,87 +9,17 @@ import matplotlib.pyplot as plt
 from dataObj import *
 
 
-class rmpSolution():
-    def __init__(self, numApproxPoints, alphas, f):
-
-        #initialize objective elements (f, alphas)
-        self.f = np.copy(f)
-        self.alphas = np.copy(alphas)
-        # initialize y vector (intensities)
-        self.y = np.array([])
-        self.nApprox = numApproxPoints
-        # initiailze D sparse matrix of dimension (0,num points)
-        self.D = spsparse.csc_matrix((self.nApprox,0))
-        # initialize solution D vector
-        self.g = np.zeros(self.nApprox)
-        self.lVec = []
-        self.rVec = []
-
-
-    def calcObjGrad(self,y):
-        self.g = self.D.dot(y)
-        diff = self.f-self.g
-        return self.evalObj(y, diff=diff), self.evalGrad(y, diff=diff)
-
-    def evalGrad(self, y, diff=None):
-        # if diff==None, then calc diff, otherwise return gradient
-        if diff==None:
-            self.g = self.D.dot(y)
-            diff = self.f-self.g
-        return -2 * self.alphas * diff
-
-    def evalObj(self,y, diff=None):
-        # if diff==None, then calc diff, otherwise return objective
-        if diff==None:
-            self.g = self.D.dot(y)
-            diff = self.f-self.g
-        return np.sum(self.alphas * (diff**2))
-
-
-    def solvePP(self, y):
-        # calc gradient
-
-        # do search
-
-        # get best row
-
-        # add aperture to model
-
-    def addAper(self, l,r):
-        # calculate row of D, add to sparse matrix
-
-        # generate y-value for y-vector
-
-        # save l and r positions
-
-        pass
-
-    def solveRMP(self):
-        pass
-        # plug into scipy.optimize
-
-        # save solution to self
-
-
-
-
-
-
-
-class cgModel():
-    def __init__(self, runData, realTimePlotting=False, realTimePlotSaving = False, startingSolutionVector = None, trueFluenceVector = None):
-
-        #Initialize Data
-
+class cgSolver():
+    def __init__(self, runData, realTimePlotting=False, realTimePlotSaving=False, trueFluenceVector=None):
+        # def __init__(self, numApproxPoints, alphas, f, sigma, approxPoints):
         self.realTimePlotting, self.realTimePlotSaving = realTimePlotting, realTimePlotSaving
         self.K, self.width, self.resolution = runData.numAper, 1.0 * runData.width, 1.0 * runData.resolution  # num apers, width of row, spacing of approx
         self.sinGap, self.sigma, self.sinScalar = 1.0 * runData.objParams[0], 1.0 * runData.sigma, 1.0 * \
                                                   runData.objParams[
                                                       1]  # shift for sin obj, scacling factor for erf, sin scaling factor
-        self.minAperWidth, self.maxAperWidth, self.aperCenterOffset = runData.minAperWidth, runData.maxAperWidth, runData.aperCenterOffset  # min/max bounds for aper width, aper center offset from edges
         self.objCalls = 0
         # Initialize values
-        self.numApproxPoints = int(self.width / self.resolution + 1)  # number of approximation points
+        self.nApprox = int(self.width / self.resolution + 1)  # number of approximation points
         self.alphas = np.copy(runData.alphas)  # copy over objective function weights
 
         # generate approximation points, including endpoints
@@ -98,39 +28,100 @@ class cgModel():
 
         # check if there is a seed target fluence, if not, default to sin
         if trueFluenceVector != None and len(self.approxPoints) == len(trueFluenceVector):
-            self.fTarget = trueFluenceVector
+            self.f = trueFluenceVector
         else:
-            self.fTarget = np.array(np.sin(self.sinScalar * self.approxPoints) + self.sinGap)
+            self.f = np.array(np.sin(self.sinScalar * self.approxPoints) + self.sinGap)
 
+        # initialize y vector (intensities)
+        self.y = np.array([])
 
-        # build solution object
+        # initiailze D sparse matrix of dimension (0,num points)
+        self.D = spsparse.csc_matrix((self.nApprox, 0))
+        # initialize solution D vector
+        self.g = np.zeros(self.nApprox)
+        self.lVec = []
+        self.rVec = []
+        self.Dtolerance = 0.0001
 
+    def calcObjGrad(self, y):
+        self.g = self.D.dot(y)
+        diff = self.f - self.g
+        return self.evalObj(y, diff=diff), self.D.transpose().dot(self.evalGrad(y, diff=diff))
 
-        # seed initial fluence solution
+    def evalGrad(self, y, diff=None):
+        # if diff==None, then calc diff, otherwise return gradient
+        if diff is None:
+            if np.size(y) > 0:
+                self.g = self.D.dot(y)
+            else:
+                self.g = np.zeros(self.nApprox)
+            diff = self.f - self.g
+        return -2. * self.alphas * diff
 
-    def solve(self):
+    def evalObj(self, y, diff=None):
+        # if diff==None, then calc diff, otherwise return objective
+        if diff is None:
+            self.g = self.D.dot(y)
+            diff = self.f - self.g
+        return np.sum(self.alphas * (diff ** 2))
+
+    def solvePP(self, y):
+        # calc gradient
+        grad = self.evalGrad(y)
+
+        # do search
+        # helper variables
+        maxSoFar, maxEndingHere, lE, rE = 0, 0, 0, 0
+        for i in range(self.nApprox):
+            maxEndingHere += grad[i]
+            if maxEndingHere > 0:
+                maxEndingHere, lE, rE = 0, i + 1, i + 1
+            if maxSoFar > maxEndingHere:
+                maxSoFar, rE = maxEndingHere, i + 1
+        # add aperture to model
+        self.addAper(lE, rE)  # rE is non-inclusive
+
+    def solve(self, aperMax):
+        self.aperMax = aperMax
+        while np.size(self.y) < self.aperMax:
+            self.solvePP(self.y)
+            self.solveRMP()
+            print 'Aperures {} of {} added, Obj: {}'.format(str(np.size(self.y)), str(self.aperMax), str(self.obj))
+
+    def addAper(self, l, r):
+        # todo addAper
+        # calculate col of D, add to sparse matrix using error function...make midpoint then write out function...truncate at very small
+        # calc Dcol along entire axis, then truncate
+        Dcol = np.zeros(self.nApprox)
+        midpoint = self.approxPoints[l] + 1.0 * (self.approxPoints[(r - 1)] - self.approxPoints[l]) / 2.
+
+        idx = self.approxPoints <= midpoint
+        Dcol[idx] += 1 + sps.erf((self.approxPoints[idx] - self.approxPoints[l]) / self.sigma)
+        idx = self.approxPoints > midpoint
+        Dcol[idx] += sps.erfc((self.approxPoints[idx] - self.approxPoints[(r - 1)]) / self.sigma)
+        Dcol[Dcol < self.Dtolerance] = 0
+        Dcol_sparse = spsparse.csc_matrix(Dcol).transpose()
+
+        if np.size(self.y) > 0:
+            self.D = spsparse.hstack([self.D, Dcol_sparse])
+        else:
+            self.D = Dcol_sparse.copy()
+
+        # generate y-value for y-vector
+        self.y = np.resize(self.y, (1, np.size(self.y) + 1))
+
+        # save l and r positions
+        self.lVec.append(l)
+        self.rVec.append(r - 1)
+
+    def solveRMP(self):
+
+        self.res = spo.minimize(self.calcObjGrad, x0=self.y.copy(), method='L-BFGS-B', jac=True,
+                                bounds=np.array([(0, None) for i in range(np.size(self.y))]),
+                                options={'ftol': 1e-8, 'disp': False})
+        self.y = self.res['x']
+        self.obj = self.res['fun']
+
+    def printSolution(self):
+        # todo write print out funciton
         pass
-
-        # build while loop for np.size(solution.y)
-            # solve PP
-                # get best aper
-                # add best aper to model
-
-            # solve RMP
-                # execute the solve command
-                # update the solution (part of the solve command)
-
-    def outputSolution(self):
-        pass
-
-        # output solution
-
-    def plotSolution(self):
-        pass
-        # plot target fluence
-
-        # for each aperture, plot onto graph
-
-        # plot sum of apertures
-
-
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 __author__ = 's162195'
 
 from erfMidpointModel import *
+from cgModel import *
 from dataObj import *
 import math
+
 
 res = 0.01
 numAper = 12
@@ -12,12 +14,22 @@ b = 2 / math.sqrt(width)
 c = 3.
 alphas = np.ones(int(width / res + 1))
 
+
+
+
+def runerf(data, initParams,RTplot = True, RTplotsaving = False):
+    mod = erfMidModel(dat, realTimePlotting=True, realTimePlotSaving=False, initializationStringAndParams=['unifmixed', 2])
+    mod.solve()
+    mod.plotSolution()
+    mod.output('testout.mat')
+
+def runcg(data, RTplot = False, RTplotsaving = False,  trueF = None):
+    mod = cgSolver(data)
+    mod.solve(data.numAper)
+
+
+
+
 dat = dataObj([c, b], res, numAper, sigma, width, alphas, [2. / width, width / 2., 1. / width])
 
-mod = erfMidModel(dat, realTimePlotting=True, realTimePlotSaving=False, initializationStringAndParams=['unifmixed', 2])
-# mod = erfMidModel(dat)
-
-# mod = erfMidModel('sin', 0.1, 30, 5, 3,0.1)
-mod.solve()
-mod.plotSolution()
-mod.output('testout.mat')
+runcg(dat)
