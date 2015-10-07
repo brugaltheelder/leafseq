cabe42d098a8c9f321a451f0461adcbb48de06ee
Troy Long <troy.long@utsouthwestern.edu>
2015-10-07 14:53:12 -0500

    added function-specific batch run

--- main.py
+++ main.py
@@ -11,15 +11,18 @@ except ImportError:
 
 # import matplotlib
 # matplotlib.use('Agg')
+import time
+
+import numpy as np
+
 from runFunctions import *
 from fluenceFunctionBuilders import *
-import numpy as np
-import time
+
 
 
 # #data testing for CG + Explicit models
 res = 0.01
-numAper = 20
+numAper = 15
 sigma = 0.075
 width = 10.
 b = 4 / math.sqrt(width)
@@ -56,7 +59,8 @@ a = np.array([width/3, width/6])
 
 #fErfVec = fGetter.erfSum(y,m,a,sigma, truncate=0.0)
 # fErfVec = fGetter.sinFunction(c,b)
-fErfVec = fGetter.unitStep(10, 4, 7, 2)
+# fErfVec = fGetter.unitStep(10, 4, 7, 2)
+fErfVec = fGetter.doubleSinfunction(1., 1., 1. / 3., 4., 4)
 
 fGetter.functionPlotter(fErfVec,1,1,1, color = 'r', blockVar=False)
 #erfInputVec = np.zeros(3*dat.numAper)
@@ -68,7 +72,7 @@ print obj, poorobj
 
 start = time.time()
 # print runerf(dat, ['unifcent', 2], RTplot=False, finalShow=True, startVec=erfInputVec)
-print runerf(dat, ['unifcent', 2], RTplot=False, finalShow=False, startVec=erfInputVec, trueFlu=fErfVec)
+print runerf(dat, ['unifcent', 2], RTplot=False, finalShow=True, startVec=erfInputVec, trueFlu=fErfVec)
 print 'finished in {} seconds'.format(str(time.time()-start))
 #runerf(dat, ['unifcent', 2], RTplot=True, finalShow=True, trueFlu=fErfVec )
 
--- runBatchByFunction.py
+++ runBatchByFunction.py
@@ -1,8 +1,8 @@
 __author__ = 'troy'
 
-
 try:
     import mkl
+
     have_mkl = True
     print("Running with MKL Acceleration")
 except ImportError:
@@ -14,6 +14,7 @@ import matplotlib
 matplotlib.use('Agg')
 
 from runFunctions import *
+from fluenceFunctionBuilders import *
 import numpy as np
 import time
 import pandas
@@ -31,6 +32,8 @@ alphas = np.ones(int(width / res + 1))
 
 dat = dataObj([c, b], res, numAper, sigma, width, alphas, [2. / width, 3. * width / 2., 1. / width], 'TestRun')
 
+printEveryThisManyIterations = 10
+
 
 # Set initial input data
 res = 0.01
@@ -40,58 +43,86 @@ alphas = np.ones(int(width / res + 1))
 minAperWidth = 0.
 maxAperWidth = width
 minAperEdgeGap = 0.
-#minAperWidth = 1./width
-#maxAperWidth = width/2.
-#minAperEdgeGap = 0.1/width
+# minAperWidth = 1./width
+# maxAperWidth = width/2.
+# minAperEdgeGap = 0.1/width
 
 
 
-# Generate data vectors
-kParam = np.arange(5, 45 + 1, 10)
-# kParam = np.arange(5,10+1, 10)
-cParam = np.arange(2,5+1, 3)
-bParam = np.arange(1./width, 13./width+1./width, 3./width)
 
-print kParam, cParam, bParam
+
+# Generate data vectors - double sin
+# outfilename = 'doublesinout.mat'
+# kParam = np.arange(5, 45 + 1, 5)
+# cParam = np.arange(2.,6.+1., 2.)
+# bParam = np.arange(1./5., 1.+1/6., 2./5.)
+# params = paramTesting()
+# params.addParam('maxAper',kParam.tolist())
+# params.addParam('sin2scale',cParam.tolist())
+# params.addParam('sin2multiplier',bParam.tolist())
+# params.genCombination()
+# print kParam, cParam, bParam
+
+
+# Generate data vectors - random step
+outfilename, order = 'stepFunctionOutOrder0.mat', 0
+# outfilename, order = 'stepFunctionOutOrder2.mat',2
+kParam = np.arange(5, 45 + 1, 5)
+cParam = np.arange(5, 15 + 1, 5)
+bParam = np.arange(3., 6 + 1, 1)
 params = paramTesting()
-params.addParam('maxAper',kParam.tolist())
-params.addParam('sinOffset',cParam.tolist())
-params.addParam('sinScalar',bParam.tolist())
+params.addParam('maxAper', kParam.tolist())
+params.addParam('numBins', cParam.tolist())
+params.addParam('minRange', bParam.tolist())
 params.genCombination()
+print kParam, cParam, bParam
+
 
 
 # todo do a run batch with width bounds
+fGetter = functionGetter(width, res)
+
+for kInd, cInd, bInd in params.combination:
+
+    # doublesin
+    # kP, cP, bP = params.getAndSaveParams(kInd, cInd, bInd)
+    # runName = params.getFilename(kInd, cInd, bInd)
+    # dat = dataObj([0, 0], res, kP, sigma, width, alphas,[minAperWidth, maxAperWidth, minAperEdgeGap], runName)
+    # fVec = fGetter.doubleSinfunction(1.,1.,bP, cP,4.)
 
 
-for kInd,cInd,bInd in params.combination:
+
+    # random step
     kP, cP, bP = params.getAndSaveParams(kInd, cInd, bInd)
     runName = params.getFilename(kInd, cInd, bInd)
-    dat = dataObj([cP, bP], res, kP, sigma, width, alphas,[minAperWidth, maxAperWidth, minAperEdgeGap], runName)
+    dat = dataObj([0, 0], res, kP, sigma, width, alphas, [minAperWidth, maxAperWidth, minAperEdgeGap], runName)
+    fVec = fGetter.unitStep(cP, bP, 7., order=order)
+
     iterObj = []
     iterRunTime = []
     iterMUs = []
 
     # run naive techniques
-    #unifcent
+    # unifcent
     start = time.time()
     obj, MUs = runerf(dat, ['unifcent', 3], RTplot=False, finalShow=False, outputName='unifcent_out.mat',
-                      closePlots=True, pTag='unifcent')
+                      closePlots=True, pTag='unifcent', trueFlu=fVec)
     iterObj.append(obj)
     iterMUs.append(MUs)
     iterRunTime.append(time.time() - start)
 
-    #unifwidth
+    # unifwidth
     start = time.time()
     obj, MUs = runerf(dat, ['unifwidth', 3], RTplot=False, finalShow=False, outputName='unifwidth_out.mat',
-                      closePlots=True, pTag='unifwidth')
+                      closePlots=True, pTag='unifwidth', trueFlu=fVec)
     iterObj.append(obj)
     iterMUs.append(MUs)
     iterRunTime.append(time.time() - start)
 
-    #unifmixed
+    # unifmixed
     start = time.time()
     obj, MUs = runerf(dat, ['unifmixed', 3], RTplot=False, finalShow=False, outputName='unifmixed_out.mat',
-                      closePlots=True, pTag='unifmixed')
+                      closePlots=True, pTag='unifmixed', trueFlu=fVec)
     iterObj.append(obj)
     iterMUs.append(MUs)
     iterRunTime.append(time.time() - start)
@@ -99,16 +130,16 @@ for kInd,cInd,bInd in params.combination:
     # run CG
     start = time.time()
     obj, erfInputVec, MUs, otherObj = runcg(dat, RTplot=False, finalShow=False, outputName='cg_out.mat',
-                                            closePlots=True, pTag='cg')
+                                            closePlots=True, pTag='cg', trueF=fVec)
     iterMUs.append(MUs)
     iterObj.append(obj)
-    iterRunTime.append(time.time()-start)
+    iterRunTime.append(time.time() - start)
 
 
     # run explicit based on cg
     start = time.time()
     obj, MUs = runerf(dat, ['unifmixed', 3], RTplot=False, finalShow=False, outputName='cgSeeded_out.mat',
-                      closePlots=True, startVec=erfInputVec, pTag='cgSeeded')
+                      closePlots=True, startVec=erfInputVec, pTag='cgSeeded', trueFlu=fVec)
     iterObj.append(obj)
     iterMUs.append(MUs)
     iterRunTime.append(time.time() - start)
@@ -116,7 +147,7 @@ for kInd,cInd,bInd in params.combination:
     # run CG with simple objective
     start = time.time()
     obj, erfInputVec, MUs, otherObj = runcg(dat, RTplot=False, finalShow=False, outputName='cgSimp_out.mat',
-                                            closePlots=True, pTag='cgSimp', simpG=True)
+                                            closePlots=True, pTag='cgSimp', simpG=True, trueF=fVec)
     iterMUs.append(MUs)
     iterObj.append(obj)
     iterRunTime.append(time.time() - start)
@@ -125,17 +156,16 @@ for kInd,cInd,bInd in params.combination:
     # run explicit based on cg
     start = time.time()
     obj, MUs = runerf(dat, ['unifmixed', 3], RTplot=False, finalShow=False, outputName='cgSimpSeeded_out.mat',
-                      closePlots=True, startVec=erfInputVec, pTag='cgSimpSeeded')
+                      closePlots=True, startVec=erfInputVec, pTag='cgSimpSeeded', trueFlu=fVec)
     iterObj.append(obj)
     iterMUs.append(MUs)
     iterRunTime.append(time.time() - start)
-
 
     params.addObjList(iterObj)
     params.addRuntimeList(iterRunTime)
     params.addMUList(iterMUs)
 
-    if len(params.obj)%10==0:
+    if len(params.obj) % printEveryThisManyIterations == 0:
         print 'Objective Function Values'
         print pandas.DataFrame(params.obj, [i for i in range(len(params.obj))],
                                ['unifcent', 'unifwidth', 'unifmixed', 'cg', 'cgSeeded', 'cgSimp', 'cgSimpSeeded'])
@@ -148,4 +178,4 @@ for kInd,cInd,bInd in params.combination:
 
 
 
-params.writeRuns('k_b_c_runsout.mat')
+params.writeRuns(outfilename)
