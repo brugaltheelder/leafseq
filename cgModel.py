corrected gradient calculation

cgModel.py
@@ -237,7 +237,7 @@ class cgSolver:
 
         # Objective function evaluation and plotting
 
-        plt.plot(self.approxPoints, self.f, 'r')
+        plt.plot(self.approxPoints, self.f, 'r', linestyle='dotted', zorder=2, linewidth=2)
 
         # plots total sequenced fluence
         g = self.D.dot(yVec)
@@ -260,7 +260,12 @@ class cgSolver:
                 plt.savefig(self.directory + '/' + 'CGiterPlotOut_' + str(self.figCounter) + '_.png')
                 self.figCounter += 1
         else:
-            plt.title('Method: CG, obj: ' + str(round(self.finalObjEval(), 5)) + ', nAper: ' + str(np.size(self.y)))
+            if self.simpleG:
+                plt.title(
+                    'Method: DLO-U Seed, obj: ' + str(round(self.finalObjEval(), 5)) + ', K: ' + str(np.size(self.y)))
+            else:
+                plt.title(
+                    'Method: DLO Seed, obj: ' + str(round(self.finalObjEval(), 5)) + ', K: ' + str(np.size(self.y)))
             plt.xlabel('Position along MLC opening')
             plt.ylabel('Fluence')
             plt.savefig(self.directory + '/' + self.runTag + '_' + self.plotTag + '.png')

erfMidpointModel.py
@@ -1,10 +1,10 @@
 __author__ = 'Troy Long'
 
-import numpy as np
 import scipy.optimize as spo
-import scipy.special as sps
 import scipy.signal as spsignal
+import scipy.special as sps
 from scipy import io
+
 # import matplotlib
 # matplotlib.use('Agg')
 import matplotlib.pyplot as plt
@@ -199,7 +199,8 @@ class erfMidModel:
 
         :returns derf: derivative of error function at x
         """
-        return 2 / np.pi * np.exp(-1 * x * x)
+        return 2 / np.sqrt(np.pi) * np.exp(-1 * x * x)
+        # return 2 / np.pi * np.exp(-1 * x * x)
 
     # returns both the objective function and the derivative for the solver
     def objGradEval(self, x):
@@ -267,7 +268,7 @@ class erfMidModel:
 
         # Objective function evaluation and plotting
 
-        plt.plot(self.approxPoints, self.fTarget, 'r')
+        plt.plot(self.approxPoints, self.fTarget, 'r', linestyle='dotted', zorder=2, linewidth=2)
 
         # Sets coherent limits to plot
         plt.ylim(0, 1.2 * max(np.max(self.finalX[0:self.K]), np.max(self.fTarget)))
@@ -310,7 +311,7 @@ class erfMidModel:
                 plt.savefig(self.directory + '/' + self.runTag + '_ERFiterPlotOut_' + str(self.figCounter) + '.png')
                 self.figCounter += 1
         else:
-            plt.title('Method: ERF, obj: ' + str(round(self.obj, 5)) + ', nAper: ' + str(self.K))
+            plt.title('Method: CLO, obj: ' + str(round(self.obj, 5)) + ', K: ' + str(self.K))
             plt.xlabel('Position along MLC opening')
             plt.ylabel('Fluence')
             plt.savefig(self.directory + '/' + self.runTag + '_' + self.plotTag + '.png')
@@ -320,13 +321,14 @@ class erfMidModel:
 
                     idx = self.approxPoints <= self.seedM[k]
                     plt.plot(self.approxPoints[idx], self.seedY[k] * 1./2. * (1 + sps.erf(
-                        (self.approxPoints[idx] - (self.seedM[k] - self.seedA[k])) / self.sigma)), 'y')
+                        (self.approxPoints[idx] - (self.seedM[k] - self.seedA[k])) / self.sigma)), 'k',
+                             linestyle='dashed', zorder=3)
                     # plot right error function
                     idx = self.approxPoints > self.seedM[k]
                     plt.plot(self.approxPoints[idx], self.seedY[k] / 2. * (
                         sps.erfc(
                             (self.approxPoints[idx] - (self.seedM[k] + self.seedA[k])) / self.sigma)),
-                             'y')
+                             'k', linestyle='dashed', zorder=3)
 
                 plt.savefig(self.directory + '/' + self.runTag + '_' + self.plotTag + '_withSeed.png')
 

main.py
@@ -11,14 +11,12 @@ except ImportError:
 
 # import matplotlib
 # matplotlib.use('Agg')
-import time
 
-import numpy as np
-
-from runFunctions import *
 from fluenceFunctionBuilders import *
+from runFunctions import *
 from stratGreedy import *
 
+np.random.seed(0)
 
 # #data testing for CG + Explicit models
 res = 0.01
@@ -65,14 +63,15 @@ a = np.array([width/3, width/6])
 fErfVec = fGetter.doubleSinfunction(1., 1., 1. / 3., 4., 4)
 #fErfVec = fGetter.erfSumRand(numAper, 0.5, 0.5, sigma, width)
 
-Kgoal = 50
+Kgoal = 200
 obj, erfInputVec, mus, k = runclingreed(dat, fErfVec, int(Kgoal *1.5), Kgoal)
 dat.setKreal(k)
 #k = stratifiedGreedy.runStratGreedy(10)
 
 #print k, erfInputVec.shape, obj, mus
 
-obj,Mus = runerf(dat, ['clinGreed', 2], RTplot=False, finalShow=True, startVec=erfInputVec, trueFlu=fErfVec, plotSeed=True, pTag=str(Kgoal))
+obj, Mus = runerf(dat, ['clinGreed', 2], RTplot=False, finalShow=True, startVec=erfInputVec, trueFlu=fErfVec,
+                  plotSeed=True, pTag=str(Kgoal), dispFreq=True)
 
 
 

runBatchByFunctionClinK.py
@@ -18,7 +18,7 @@ import numpy as np
 import time
 import pandas
 
-
+np.random.seed(seed=1)
 
 # #data testing for CG + Explicit models
 

stratGreedy.py
@@ -3,11 +3,11 @@ Created on 29 October 2015
 @author: Troy Long
 """
 
+import warnings
 
+import matplotlib.pyplot as plt
 import numpy as np
 import scipy.cluster as spc
-import matplotlib.pyplot as plt
-import warnings
 import scipy.special as sps
 from scipy import io
 
@@ -165,9 +165,9 @@ class stratGreedy(object):
 
     def plotStrat(self):
         """Plots the initial Fluence, stratified fluence, and aperture fluences"""
-        
-        plt.plot(self.approxPoints,self.f,'r')
-        plt.plot(self.approxPoints,self.f_strat_latest,'c')
+
+        plt.plot(self.approxPoints, self.f, 'r', linestyle='dotted', zorder=2, linewidth=2)
+        # plt.plot(self.approxPoints,self.f_strat_latest,'c', linestyle = '')
         
         plt.ylim(0, 1.2 * max(np.max(self.y), np.max(self.f)))
         plt.xlim(0, self.width)
@@ -179,8 +179,10 @@ class stratGreedy(object):
             gHolder = self.y[k] * 0.5 * (sps.erf((self.approxPoints - (self.m[k] - self.a[k]))/(self.sigma)) - sps.erf((self.approxPoints - (self.m[k] + self.a[k]))/(self.sigma)))
             plt.plot(self.approxPoints, gHolder,'b')
             g+= gHolder
-        plt.plot(self.approxPoints,g,'g')
-        plt.title('Method: Clinical Greedy, obj: ' + str(round(self.getObj(g),5)) + ', K: ' + str(self.y.shape[0]) + ', L: ' + str(self.L))
+        plt.plot(self.approxPoints, g, 'g', linestyle='solid')
+        plt.title(
+            'Method: Trad Seed, obj: ' + str(round(self.getObj(g), 5)) + ', K: ' + str(self.y.shape[0]) + ', L: ' + str(
+                self.L))
         plt.xlabel('Position along MLC opening')
         plt.ylabel('Fluence')
         plt.savefig(self.directory + '/' + self.runTag + '_' + self.plotTag + '.png')